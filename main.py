6ed4083c3a8627520a8b9f3305cf9ee468c136fa
ManuelAlbrecht <info@manuel-albrecht.com>
2025-04-04 17:55:41 +0530

    Update main.py: changed to port to 5050 to run in DO

--- main.py
+++ main.py
@@ -280,7 +280,7 @@ if __name__ == '__main__':
     
     app.run(
         host='0.0.0.0', 
-        port=int(os.getenv('PORT', 5000)), 
+        port=int(os.getenv('PORT', 5050)), 
         debug=os.getenv('FLASK_DEBUG', False)
     )
 
